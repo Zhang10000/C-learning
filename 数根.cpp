efecf4430ef75650905bbd6fdba56307ad1a3f7b
Zhang10000 <52526209+Zhang10000@users.noreply.github.com>
2019-07-29 22:23:39 +0800

    Add files via upload

---
+++ 数根.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main() {
+	string n;
+	while (cin >> n) {
+		int cur = 0;
+		for (auto& c : n) cur += c - '0';
+		n = to_string(cur);
+		while (n.size() > 1)
+		{
+			cur = 0;
+			for (auto& c : n) cur += c - '0';
+			n = to_string(cur);
+		}
+		cout << n << endl;
+	}
+	return 0;
+}
