c9f80c5b8861596c8da41c6ea164565af5e2392f
Zhang10000 <52526209+Zhang10000@users.noreply.github.com>
2019-08-10 20:16:21 +0800

    Update 动规：路径.cpp

--- 动规：路径.cpp
+++ 动规：路径.cpp
@@ -1,20 +1,19 @@
-//#include<iostream>
-//#include<vector>
-//using namespace std;
-//
-//void find_ss(int n, int m) {
-//	vector<vector<int> > a(n + 1, vector<int>(m + 1, 1));
-//	for (int i = 1; i <= n; ++i) {
-//		for (int j = 1; j <= m; ++j) {
-//			a[i][j] = a[i - 1][j] + a[i][j - 1];
-//		}
-//	}
-//	cout << a[n][m] << endl;
-//}
-//
-//int main567789() {
-//	int n, m;
-//	cin >> n >> m;
-//	find_ss(n, m);
-//	return 0;
-//}
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void find_ss(int n, int m) {
+	vector<vector<int> > a(n + 1, vector<int>(m + 1, 1));
+	for (int i = 1; i <= n; ++i) {
+		for (int j = 1; j <= m; ++j) {
+			a[i][j] = a[i - 1][j] + a[i][j - 1];
+		}
+	}
+	cout << a[n][m] << endl;
+}
+int main567789() {
+	int n, m;
+	cin >> n >> m;
+	find_ss(n, m);
+	return 0;
+}
