bef7765c145a89e8409660aaf2bd290791776465
Zhang10000 <52526209+Zhang10000@users.noreply.github.com>
2019-08-17 23:04:48 +0800

    Add files via upload

---
+++ Emacs计算器.cpp
@@ -0,0 +1,36 @@
+#include<vector>
+#include<iostream>
+using namespace std;
+int main() {
+	int n;
+	while (cin >> n) {
+		vector<int>num;
+		vector<char>ema;
+		char a;
+		for (int i = 0; i < n; i++) {
+			cin >> a;
+			if (a == '+' || a == '-' || a == '*' || a == '/')
+				ema.push_back(a);
+			else 
+				num.push_back(a - '0');
+		}
+		for (int i = 0; i < ema.size(); ++i) {
+			switch (ema[i]){
+			case '*':num[i] *= num[i + 1];
+			case '/':num[i] /= num[i + 1];
+			default:
+				continue;
+			}
+		}
+		for (int i = 0; i < ema.size(); ++i) {
+			switch (ema[i]) {
+			case '+':num[i] += num[i + 1];
+			case '-':num[i] -= num[i + 1];
+			default:
+				continue;
+			}
+		}
+		cout << num[0] << endl;
+	}
+	return 0;
+}
