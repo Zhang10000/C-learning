de0e1a8ab05f5c1c733c1af601fda8ed1676913c
Zhang10000 <52526209+Zhang10000@users.noreply.github.com>
2020-03-17 21:51:53 +0800

    Create 完美转发.cpp

---
+++ 完美转发.cpp
@@ -0,0 +1,24 @@
+template<typename T>
+void print(T& t) {
+	cout << "lvalue" << endl;
+}
+template<typename T>
+void print(T&& t) {
+	cout << "rvalue" << endl;
+}
+
+template<typename T>
+void TestForward(T && v) {
+	print(v);
+	print(std::forward<T>(v));
+	print(std::move(v));
+	cout << endl;
+}
+
+int main() {
+	TestForward(1);
+	int x = 1;
+	TestForward(x);
+	TestForward(std::forward<int>(x));
+	return 0;
+}
